8136483f1c53ecfee24888ef782df31d84688525
L_kanglin <821953655@qq.com>
2020-02-13 17:06:31 +0800

    20200213 新加分布式配置中心，完成最后一个组件的学习

---
+++ microstarcloud-config-client-3355/src/main/java/com/microstar/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,16 @@
+package com.microstar.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by PVer on 2020/2/13.
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+}
